8f45d6e6f0645435a5a5871897ba01cef32a3704
SACHIN KUMAR MUDULI <142773170+SACHIN-IITM@users.noreply.github.com>
2023-12-05 22:26:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,14 @@ import LotteryGame from './lotteryGame';
 import { sum } from './helper';
 
 function App() {
-
   let winCondition = (ticket) => {
-    return sum(ticket) === 15;
+    return sum(ticket) === 12;
   }
-
-
   return (
     <>
-      <LotteryGame n={3} winCondition={winCondition}  ></LotteryGame>
+      <LotteryGame n={3} winCondition={winCondition} ></LotteryGame>
     </>
   );
-}
+};
 
 export default App;
