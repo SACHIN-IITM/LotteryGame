f64d8abe1e2cc02b46ced66e4d66aede01d247f6
SACHIN KUMAR MUDULI <142773170+SACHIN-IITM@users.noreply.github.com>
2023-12-09 20:14:10 +0530

    Update Ticket.jsx

--- src/Ticket.jsx
+++ src/Ticket.jsx
@@ -7,7 +7,7 @@ export default function Ticket({ticket}) {
             <p> Ticket </p>
             {ticket.map((num, idx) => (
                 <TicketNum num={num} key={idx}/>
-            ))}
+            ))};
         </div>
     );
 };
